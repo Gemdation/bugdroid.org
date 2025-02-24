498e99678b2b788b45d9a65bbd5fd07a01aa4d15
Gemdation <166338329+Gemdation@users.noreply.github.com>
2025-02-24 16:35:01 -0600

    File rearrange, new pages, new slideshow thing

---
+++ meta/slides.js
@@ -0,0 +1,11 @@
+// Multiple slideshows can't be on one page.
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slides');
+function showSlide(index) {slides.forEach((slide, i) => {slide.style.display = (i === index) ? 'block' : 'none';});}
+function nextSlide() {currentSlide = (currentSlide + 1) % slides.length;showSlide(currentSlide);}
+function prevSlide() {currentSlide = (currentSlide - 1 + slides.length) % slides.length; showSlide(currentSlide);} showSlide(currentSlide);
+
+// <div class="cotaniner">
+// <img class="slides">
+// <button onclick="prevSlide()">◀</button>
+// <button onclick="nextSlide()">▶</button>
