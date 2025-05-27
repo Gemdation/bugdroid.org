d2cfff34d689c10f45d8802fcdacba9b96253747
Gemdation <gemdation@aol.com>
2025-05-26 21:32:13 -0500

    Added more YouTube items and added a to-do list

--- meta/product-db-youtube.js
+++ meta/product-db-youtube.js
@@ -1,9 +1,385 @@
-// Used for the YouTube merch archives. 
+// Used for the YouTube merch archives.
 const productData = {
+  "jotter-pad": {
+    name: "YouTube Jotter Task Pad",
+    maker: "Robertson Marketing",
+    year: "2020",
+    id: "GGOEYOLB151999",
+    price: "$3.00",
+    synopsis: "A sustainable way to take notes. This reporter-size YouTube notebook has a recycled cardboard cover and a matching pen with a recycled cardboard barrel that fits in the elastic pen loop. 1% of your purchase goes to nonprofits who protect our Earth.",
+    images: [
+      "media/products/GGOEYOLB151999.jpg",
+      "media/products/GGOEYOLB151999-2.jpg",
+      "media/products/GGOEYOLB151999-3.jpg"
+    ]
+  },
+  "zip-hoodie": {
+    name: "YouTube Standards Zip Hoodie Black",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYXXX1207",
+    price: "$60.00",
+    synopsis: "The softness of this YouTube full zip fleece hoodie will keep you comfortable and warm.",
+    images: [
+      "media/products/GGOEYXXX1207.jpg",
+      "media/products/GGOEYXXX1207-2.jpg",
+      "media/products/GGOEYXXX1207-3.jpg"
+    ]
+  },
+  "icon-tee-grey": {
+    name: "YouTube Icon Tee Grey",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYXXX1203",
+    price: "$22.00",
+    synopsis: "This ultra soft YouTube tee is the perfect shirt for relaxing and watching your favorite videos.",
+    images: [
+      "media/products/GGOEYXXX1203.jpg",
+      "media/products/GGOEYXXX1203-2.jpg",
+      "media/products/GGOEYXXX1203-3.jpg"
+    ]
+  },
+  "icon-tee-charcoal": {
+    name: "YouTube Icon Tee Charcoal",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYXXX1202",
+    price: "$22.00",
+    synopsis: "This ultra soft YouTube tee is the perfect shirt for relaxing and watching your favorite videos.",
+    images: [
+      "media/products/GGOEYXXX1202.jpg",
+      "media/products/GGOEYXXX1202-2.jpg",
+      "media/products/GGOEYXXX1202-3.jpg"
+    ]
+  },
+  "iconic-pin": {
+    name: "YouTube Iconic Play Pin",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYCBR138999",
+    price: "$8.00",
+    synopsis: "Wear this pin to show you're a player.", // lmfao
+    images: [
+      "media/products/GGOEYCBR138999.jpg",
+      "media/products/GGOEYCBR138999-2.jpg",
+      "media/products/GGOEYCBR138999-3.jpg"
+    ]
+  },
+  "icon-bundle": {
+    name: "YouTube Icon Bundle",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYCBA128999",
+    price: "$42.00",
+    synopsis: "Show your love for YouTube with this discounted bundle of essential YouTube merchandise.",
+    images: [
+      "media/products/GGOEYCBA128999.jpg"
+    ]
+  },
+  "transmission-journal-red": {
+    name: "YouTube Transmission Journal Red",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYDWB123299",
+    price: "$16.00",
+    synopsis: "This Youtube Transmission Journal has a smooth hardcover. It comes in the classic Youtube red.",
+    images: [
+      "media/products/GGOEYOCR125599.jpg",
+      "media/products/GGOEYOCR125599-2.jpg",
+      "media/products/GGOEYOCR125599-3.jpg"
+    ]
+  },
+  "play-mug": {
+    name: "YouTube Play Mug",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYDWB123299",
+    price: "$13.00",
+    synopsis: "Whatever your choice of hot beverage may be it will taste great in this 15oz dual color stoneware mug.",
+    images: [
+      "media/products/GGOEYDWB123299.jpg",
+      "media/products/GGOEYDWB123299-2.jpg",
+      "media/products/GGOEYDWB123299-3.jpg"
+    ]
+  },
+  "strap-hat": {
+    name: "YouTube Leather Strap Hat Black",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYHPB121110",
+    price: "$17.00",
+    synopsis: "Stay protected from the sun and show off your appreciation for YouTube with this black adjustable twill hat.",
+    images: [
+      "media/products/GGOEYHPB121110.jpg",
+      "media/products/GGOEYHPB121110-2.jpg",
+      "media/products/GGOEYHPB121110-3.jpg"
+    ]
+  },
+  "buoy-bottle": {
+    name: "YouTube Buoy Bottle",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEYDHR117299",
+    price: "$8.00",
+    synopsis: "Stay hydrated with this dual opening YouTube water bottle.",
+    images: [
+      "media/products/GGOEYDHR117299.jpg",
+      "media/products/GGOEYDHR117299-2.jpg",
+      "media/products/GGOEYDHR117299-3.jpg"
+    ]
+  },
+  "small-sticker-sheet": {
+    name: "YouTube Small Sticker Sheet",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYFKQ105899",
+    price: "$2.99",
+    synopsis: "4” x 6” custom YouTube sticker sheet. Sticker sheet comes with seven separate YouTube stickers.",
+    images: [
+      "media/products/GGOEYFKQ105899.jpg",
+      "media/products/GGOEYFKQ105899-2.jpg",
+      "media/products/GGOEYFKQ105899-3.jpg"
+    ]
+  },
+  "crew-socks-summer": {
+    name: "YouTube Crew Socks Summer 18",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYAEA105610",
+    price: "$15.99",
+    synopsis: "Custom branded YouTube crew socks. 76% cotton, 22% nylon, 2% spandex. One size fits most.",
+    images: [
+      "media/products/GGOEYAEA105610.jpg",
+      "media/products/GGOEYAEA105610-2.jpg",
+      "media/products/GGOEYAEA105610-3.jpg"
+    ]
+  },
+  "tritan-sport-bottle": {
+    name: "YouTube Tritan Sport Bottle 23oz",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYDHJ104099",
+    price: "$13.99",
+    synopsis: "This durable 23oz Tritan bottle is shatter, stain, and odor resistant; making it a perfect liquid companion.",
+    images: [
+      "media/products/GGOEYDHJ104099.jpg",
+      "media/products/GGOEYDHJ104099-2.jpg",
+      "media/products/GGOEYDHJ104099-3.jpg"
+    ]
+  },
+  "twill-sandwich-cap": {
+    name: "YouTube Twill Sandwich Cap Black",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYHBB102199",
+    price: "$12.99",
+    synopsis: "This YouTube Twill Sandwich Cap will not only keep the sun out of your eyes but will also keep you looking stylish.",
+    images: [
+      "media/products/GGOEYHBB102199.jpg",
+      "media/products/GGOEYHBB102199-2.jpg",
+      "media/products/GGOEYHBB102199-3.jpg"
+    ]
+  },
+  "pen-two-pack": {
+    name: "YouTube Pen 2-Pack",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYOAA101499",
+    price: "$3.99",
+    synopsis: "The soft touch surface and fine point gel tip of this YouTube Pen will make this your go-to pen for any of your writing needs.",
+    images: [
+      "media/products/GGOEYOAA101499.jpg",
+      "media/products/GGOEYOAA101499-2.jpg",
+      "media/products/GGOEYOAA101499-3.jpg"
+    ]
+  },
+  "flex-journal": {
+    name: "YouTube Flex Journal Black",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYOCB100499",
+    price: "$7.99",
+    synopsis: " This YouTube Flex Journal has 50 lined sheets waiting for you to write down your imaginations and doodles.",
+    images: [
+      "media/products/GGOEYOCB100499.jpg",
+      "media/products/GGOEYOCB100499-2.jpg",
+      "media/products/GGOEYOCB100499-3.jpg"
+    ]
+  },
+  "gear-cap-bottle": {
+    name: "YouTube 25 oz Gear Cap Bottle Black",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYDHB098499",
+    price: "$26.99",
+    synopsis: "Take your favorite hot or cold drink anywhere with this YouTube 25oz Gear Cap Bottle. The carrying handle lid makes it convenient to take with you to your next meeting or workout.",
+    images: [
+      "media/products/GGOEYDHB098499.jpg",
+      "media/products/GGOEYDHB098499-2.jpg",
+      "media/products/GGOEYDHB098499-3.jpg"
+    ]
+  },
+  "transmission-journal": {
+    name: "YouTube Transmission Journal Black",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYOCB092699",
+    price: "$15.99",
+    synopsis: "This YouTube Transmission Journal is a convenient tool to collect your thoughts and doodles throughout the day.",
+    images: [
+      "media/products/GGOEYOCB092699.jpg",
+      "media/products/GGOEYOCB092699-2.jpg",
+      "media/products/GGOEYOCB092699-3.jpg"
+    ]
+  },
+  "transmission-bottle": {
+    name: "YouTube Transmission Bottle Black",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYDHB086099",
+    price: "$23.99",
+    synopsis: "Keep your favorite drink cold during those long workouts with this YouTube Transmission Bottle. 17 oz.",
+    images: [
+      "media/products/GGOEYDHB086099.jpg",
+      "media/products/GGOEYDHB086099-2.jpg",
+      "media/products/GGOEYDHB086099-3.jpg"
+    ]
+  },
+  "icon-pullover": {
+    name: "YouTube Icon Pullover Black",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEYXXX0938",
+    price: "$59.99",
+    synopsis: "This YouTube Transmission Journal is a convenient tool to collect your thoughts and doodles throughout the day.",
+    images: [
+      "media/products/GGOEYXXX0938.jpg",
+      "media/products/GGOEYXXX0938-2.jpg",
+      "media/products/GGOEYXXX0938-3.jpg"
+    ]
+  },
+  "wordmark-crew-grey": {
+    name: "YouTube Wordmark Crew Grey",
+    maker: "Robertson Marketing",
+    year: "2018",
+    id: "GGOEGXXX0921",
+    price: "$51.99",
+    synopsis: "Kick back and relax in this comfortable YouTube sweatshirt. Unisex sizing.",
+    images: [
+      "media/products/GGOEYXXX0937.jpg",
+      "media/products/GGOEYXXX0937-2.jpg",
+      "media/products/GGOEYXXX0937-3.jpg"
+    ]
+  },
+  "three-lines-tee": {
+    name: "YouTube 3 Lines Tee Grey",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEGXXX0921",
+    price: "$21.99",
+    synopsis: "Stay innovating with this YouTube tee made of soft tri-blend material. Unisex Sizing.",
+    images: [
+      "media/products/GGOEGXXX0921.jpg",
+      "media/products/GGOEGXXX0921-2.jpg",
+      "media/products/GGOEGXXX0921-3.jpg",
+      "media/products/GGOEGXXX0921-4.jpg"
+    ]
+  },
+  "favorite-tee-white": {
+    name: "YouTube Women's Favorite Tee White",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEGXXX0918",
+    price: "$21.99",
+    synopsis: "Keep it simple with the new logo wearing this Youtube tee.",
+    images: [
+      "media/products/GGOEGXXX0919.jpg",
+      "media/products/GGOEGXXX0919-2.jpg",
+      "media/products/GGOEGXXX0919-3.jpg"
+    ]
+  },
+  "tee-black": {
+    name: "YouTube Tee Black",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEGXXX0918",
+    price: "$21.99",
+    synopsis: "Keep it simple with the new logo wearing this YouTube tee. Unisex sizing.",
+    images: [
+      "media/products/GGOEGXXX0918.jpg",
+      "media/products/GGOEGXXX0918-2.jpg",
+      "media/products/GGOEGXXX0918-3.jpg"
+    ]
+  },
+  "kids-onesie-white": {
+    name: "YouTube Kids Onesie White",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEYXXX0897",
+    price: "$25.99",
+    synopsis: "This 100% cotton Youtube Onesie for your baby will keep them comfortable with non-binding sleeves for full freedom of movement.",
+    images: [
+      "media/products/GGOEYXXX0897.jpg",
+      "media/products/GGOEYXXX0897-2.jpg",
+      "media/products/GGOEYXXX0897-3.jpg"
+    ]
+  },
+  "kids-tee-black": {
+    name: "YouTube Kids Tee Black",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEYXXX0896",
+    price: "$19.99",
+    synopsis: "This 100% cotton Youtube short sleeve t-shirt is perfect for kids to wear to school or while playing.",
+    images: [
+      "media/products/GGOEYXXX0896.jpg",
+      "media/products/GGOEYXXX0896-2.jpg",
+      "media/products/GGOEYXXX0896-3.jpg"
+    ]
+  },
+  "kids-tee-white": {
+    name: "YouTube Kids Tee White",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEYXXX0895",
+    price: "$19.99",
+    synopsis: "This 100% cotton Youtube short sleeve t-shirt is perfect for kids to wear to school or while playing.",
+    images: [
+      "media/products/GGOEYXXX0895.jpg",
+      "media/products/GGOEYXXX0895-2.jpg",
+      "media/products/GGOEYXXX0895-3.jpg"
+    ]
+  },
+  "fleece-hoodie-womens": {
+    name: "YouTube Women's Fleece Hoodie Black",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEGAAX0732",
+    price: "$55.99",
+    synopsis: "Vintage styling gives this YouTube hoodie a timeless appeal. With a full zipper and stand-out drawstrings.",
+    images: [
+      "media/products/GGOEGAAX0732.jpg",
+      "media/products/GGOEGAAX0732-2.jpg"
+    ]
+  },
+  "badge-tee-charcoal-womens": {
+    name: "YouTube Women's Short Sleeve Hero Tee Charcoal",
+    maker: "Robertson Marketing",
+    year: "2017",
+    id: "GGOEGAAX0295",
+    price: "$18.99",
+    synopsis: "A YouTube t-shirt for women made of luxurious tri-belnd jersey fabric, this Made in the USA tee can't be beat for style and comfort.",
+    // "tri-belnd" is not a mistake, it appeared on the official synopsis
+    images: [
+      "media/products/GGOEGAAX0295.jpg",
+      "media/products/GGOEGAAX0295-2.jpg",
+      "media/products/GGOEGAAX0295-3.jpg"
+    ]
+  },
   "hero-tee-charcoal-womens": {
     name: "YouTube Women's Short Sleeve Hero Tee Charcoal",
     maker: "Robertson Marketing",
-    year: "~2017",
+    year: "2017",
     id: "GGOEGAAX0290",
     price: "$18.99",
     synopsis: "They don't call it the favorite for nothing. Experience the 100% cotton comfort.",
@@ -16,7 +392,7 @@ const productData = {
   "long-tee-charcoal": {
     name: "YouTube Men's Long & Lean Tee Charcoal",
     maker: "Robertson Marketing",
-    year: "~2017",
+    year: "2017",
     id: "GGOEGAAX0330",
     price: "$19.99",
     synopsis: "Tri-blend jersey fabric makes this Android t-shirt easy to wear and easy to move in. USA made with logo in the 'badge' location.",
@@ -29,7 +405,7 @@ const productData = {
   "hero-tee-charcoal": {
     name: "YouTube Men's Short Sleeve Hero Tee Charcoal",
     maker: "Robertson Marketing",
-    year: "~2017",
+    year: "2017",
     id: "GGOEGAAX0325",
     price: "$17.99",
     synopsis: "Made of soft tri-blend jersey fabric, this amazing YouTube t-shirt proudly bears the YouTube logo front and center. Made in USA.",
