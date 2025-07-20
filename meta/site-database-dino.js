835677a996f74b7fb62ca02018a6f0cee7a12aaf
Gemdation <gem@techie.com>
2025-07-20 11:01:56 -0500

    Setting up for the Chrome Dino collection

---
+++ meta/site-database-dino.js
@@ -0,0 +1,30 @@
+// Used for the Chrome Dino merch archives.
+
+const productData = {
+  "dino-accessory": {
+    name: "Chrome Dino YouTube Skater Accessory Pack",
+    maker: "Robertson Marketing",
+    year: "2024",
+    id: "GGOEGFBA241299",
+    price: "$16.00",
+    synopsis: "Chrome Dino has a big attitude but an even bigger boombox. This T-Rex is sure to shred all expectations with their YouTube skateboard, one kickflip at a time.",
+    images: [
+      "media/products/youtube/GGOEGFBA241299.webp",
+      "media/products/youtube/GGOEGFBA241299-2.webp",
+      "media/products/youtube/GGOEGFBA241299-3.webp"
+    ]
+  },
+  "express-sweatshirt": {
+    name: "YouTube Expressive Sweatshirt",
+    maker: "Robertson Marketing",
+    year: "~2021",
+    id: "GGOEYXXX1799",
+    price: "$63.00",
+    synopsis: "Express yourself while wearing this awesome limited-edition graphic YouTube Sweatshirt! Even better, it’s made from eco-friendly fabric.",
+    images: [
+      "media/products/youtube/GGOEYXXX1799.webp",
+      "media/products/youtube/GGOEYXXX1799-2.webp",
+      "media/products/youtube/GGOEYXXX1799-3.webp"
+    ]
+  },
+};
