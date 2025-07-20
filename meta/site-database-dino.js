244db8ef962db31926e145f24854b2e6a60216ab
Gemdation <gem@techie.com>
2025-07-20 18:20:24 -0500

    Adding Dino figurines

--- meta/site-database-dino.js
+++ meta/site-database-dino.js
@@ -1,30 +1,117 @@
 // Used for the Chrome Dino merch archives.
 
 const productData = {
-  "dino-accessory": {
+  "dino-accessory-summer": {
+    name: "Chrome Dino Hot Dino Summer Accessory Pack",
+    maker: "Dyzplastic",
+    year: "2025",
+    id: "[none]",
+    price: "$14.00",
+    synopsis: "It’s hot and we are not talking about the weather! Chrome Dino is sporting a must-have bucket hat with a matching Google tote and a camp shirt bearing Chrome Dino’s likeness.",
+    images: [
+      "media/products/dino/dino-summer-pack-1.webp",
+      "media/products/dino/dino-summer-pack-2.webp"
+    ]
+  },
+  "dino-accessory-cozy": {
+    name: "Chrome Dino Warm and Cozy Accessory Pack",
+    maker: "Dyzplastic",
+    year: "2024",
+    id: "GGL2045",
+    price: "£14.00",
+    synopsis: "Get your Dino warm and cosy with this knit cap, scarf and a mug of hot cocoa.",
+    images: [
+      "media/products/dino/GGL2045-1.webp",
+      "media/products/dino/GGL2045-2.webp",
+      "media/products/dino/GGL2045-3.webp",
+      "media/products/dino/GGL2045-4.webp"
+    ]
+  },
+  "dino-accessory-aloha": {
+    name: "Chrome Dino Aloha Accessory Pack",
+    maker: "Dyzplastic",
+    year: "2024",
+    id: "GGL2045",
+    price: "$16.00",
+    synopsis: "Chrome Dino says Aloha! With a straw hat and Hawaiian shirt billowing in the breeze, this T-Rex is ready to serenade you with some sweet island melodies.",
+    images: [
+      "media/products/dino/GGOEGFBA241999-1.webp",
+      "media/products/dino/GGOEGFBA241999-2.webp",
+      "media/products/dino/GGOEGFBA241999-3.webp"
+    ]
+  },
+  "dino-accessory-youtube": {
     name: "Chrome Dino YouTube Skater Accessory Pack",
-    maker: "Robertson Marketing",
+    maker: "Dyzplastic",
     year: "2024",
     id: "GGOEGFBA241299",
     price: "$16.00",
     synopsis: "Chrome Dino has a big attitude but an even bigger boombox. This T-Rex is sure to shred all expectations with their YouTube skateboard, one kickflip at a time.",
     images: [
-      "media/products/youtube/GGOEGFBA241299.webp",
-      "media/products/youtube/GGOEGFBA241299-2.webp",
-      "media/products/youtube/GGOEGFBA241299-3.webp"
+      "media/products/dino/GGOEGFBA241299-1.webp",
+      "media/products/dino/GGOEGFBA241299-2.webp",
+      "media/products/dino/GGOEGFBA241299-3.webp"
     ]
   },
-  "express-sweatshirt": {
-    name: "YouTube Expressive Sweatshirt",
-    maker: "Robertson Marketing",
-    year: "~2021",
-    id: "GGOEYXXX1799",
-    price: "$63.00",
-    synopsis: "Express yourself while wearing this awesome limited-edition graphic YouTube Sweatshirt! Even better, it’s made from eco-friendly fabric.",
+  "dino-accessory-party": {
+    name: "Chrome Dino Party Time Accessory Pack",
+    maker: "Dyzplastic",
+    year: "2024",
+    id: "GGOEGFDA213699",
+    price: "$14.00",
+    synopsis: "Every day is a day to celebrate and Chrome Dino is prepared! Decked out with a yay headband, floating balloon and a mystery gift, a party is a phone call away.",
     images: [
-      "media/products/youtube/GGOEYXXX1799.webp",
-      "media/products/youtube/GGOEYXXX1799-2.webp",
-      "media/products/youtube/GGOEYXXX1799-3.webp"
+      "media/products/dino/GGOEGFDA213699-1.webp",
+      "media/products/dino/GGOEGFDA213699-2.webp",
+      "media/products/dino/GGOEGFDA213699-3.webp"
+    ]
+  },
+  "dino-accessory-googler": {
+    name: "Chrome Dino Googler Accessory Pack",
+    maker: "Dyzplastic",
+    year: "2024",
+    id: "GGOEGFDA213799",
+    price: "$14.00",
+    synopsis: "Chrome Dino is ready for work! Decked out with a lanyard, handy backpack and the world-famous Noogler cap, this little dino is ready to tackle anything.",
+    images: [
+      "media/products/dino/GGOEGFDA213799-1.webp",
+      "media/products/dino/GGOEGFDA213799-2.webp",
+      "media/products/dino/GGOEGFDA213799-3.webp"
+    ]
+  },
+  "dino-set-dark": {
+    name: "Chrome Dino Dark Mode Set",
+    maker: "Dyzplastic",
+    year: "2019",
+    id: "GGOEGABQ170299",
+    price: "$28.00",
+    synopsis: "Internet is out again! Our dino is back in a special 'Dark Mode' edition featuring a glossy black bases and blue glow in the dark material!",
+    images: [
+      "media/products/dino/GGOEGABQ170299-1.webp",
+      "media/products/dino/GGOEGABQ170299-2.webp",
+      "media/products/dino/GGOEGABQ170299-3.webp",
+      "media/products/dino/GGOEGABQ170299-4.webp",
+      "media/products/dino/GGOEGABQ170299-5.webp",
+      "media/products/dino/GGOEGABQ170299-6.webp",
+      "media/products/dino/GGOEGABQ170299-7.webp"
+    ]
+  },
+  "dino-set": {
+    name: "Chrome Dino Set",
+    maker: "Dyzplastic",
+    year: "2019",
+    id: "GGOEGABJ125299",
+    price: "$28.00",
+    synopsis: "The dreaded ERR_INTERNET_DISCONNECTED! Do I call tech support? Do I scream? Do I... go outside? Oh look, a cute dinosaur.. this should keep me busy until someone else figures it out.",
+    images: [
+      "media/products/dino/GGOEGABJ125299-1.webp",
+      "media/products/dino/GGOEGABJ125299-2.webp",
+      "media/products/dino/GGOEGABJ125299-3.webp",
+      "media/products/dino/GGOEGABJ125299-4.webp",
+      "media/products/dino/GGOEGABJ125299-5.webp",
+      "media/products/dino/GGOEGABJ125299-6.webp",
+      "media/products/dino/GGOEGABJ125299-7.webp",
+      "media/products/dino/GGOEGABJ125299-8.webp"
     ]
   },
 };
