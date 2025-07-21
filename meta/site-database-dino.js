a19684f59d1f050087809a8956738cd26257230f
Gemdation <gem@techie.com>
2025-07-21 11:51:23 -0500

    More Dino products, added button to home page

--- meta/site-database-dino.js
+++ meta/site-database-dino.js
@@ -1,19 +1,7 @@
 // Used for the Chrome Dino merch archives.
 
 const productData = {
-  "dino-accessory-summer": {
-    name: "Chrome Dino Hot Dino Summer Accessory Pack",
-    maker: "Dyzplastic",
-    year: "2025",
-    id: "[none]",
-    price: "$14.00",
-    synopsis: "It’s hot and we are not talking about the weather! Chrome Dino is sporting a must-have bucket hat with a matching Google tote and a camp shirt bearing Chrome Dino’s likeness.",
-    images: [
-      "media/products/dino/dino-summer-pack-1.webp",
-      "media/products/dino/dino-summer-pack-2.webp"
-    ]
-  },
-  "dino-accessory-cozy": {
+  "figure-accessory-cozy": {
     name: "Chrome Dino Warm and Cozy Accessory Pack",
     maker: "Dyzplastic",
     year: "2024",
@@ -27,7 +15,19 @@ const productData = {
       "media/products/dino/GGL2045-4.webp"
     ]
   },
-  "dino-accessory-aloha": {
+  "figure-accessory-summer": {
+    name: "Chrome Dino Hot Dino Summer Accessory Pack",
+    maker: "Dyzplastic",
+    year: "2025",
+    id: " ",
+    price: "$14.00",
+    synopsis: "It’s hot and we are not talking about the weather! Chrome Dino is sporting a must-have bucket hat with a matching Google tote and a camp shirt bearing Chrome Dino’s likeness.",
+    images: [
+      "media/products/dino/dino-summer-pack-1.webp",
+      "media/products/dino/dino-summer-pack-2.webp"
+    ]
+  },
+  "figure-accessory-aloha": {
     name: "Chrome Dino Aloha Accessory Pack",
     maker: "Dyzplastic",
     year: "2024",
@@ -40,7 +40,7 @@ const productData = {
       "media/products/dino/GGOEGFBA241999-3.webp"
     ]
   },
-  "dino-accessory-youtube": {
+  "figure-accessory-youtube": {
     name: "Chrome Dino YouTube Skater Accessory Pack",
     maker: "Dyzplastic",
     year: "2024",
@@ -53,7 +53,33 @@ const productData = {
       "media/products/dino/GGOEGFBA241299-3.webp"
     ]
   },
-  "dino-accessory-party": {
+  "surfing-patch": {
+    name: "Chrome Dino Surfing Patch",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGCBQ215799",
+    price: "$5.50",
+    synopsis: "Chrome Dino is amped to crest some clean waves and get barrelled. However, Chrome Dino is not too stoked about those pesky sharks&watch out or wipeout!",
+    images: [
+      "media/products/dino/GGOEGCBQ215799-1.webp",
+      "media/products/dino/GGOEGCBQ215799-2.webp",
+      "media/products/dino/GGOEGCBQ215799-3.webp"
+    ]
+  },
+  "recycled-backpack": {
+    name: "Chrome Dino Recycled Backpack",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGBRB212599",
+    price: "$57.00",
+    synopsis: " ",
+    images: [
+      "media/products/dino/GGOEGBRB212599-1.webp",
+      "media/products/dino/GGOEGBRB212599-2.webp",
+      "media/products/dino/GGOEGBRB212599-3.webp"
+    ]
+  },
+  "figure-accessory-party": {
     name: "Chrome Dino Party Time Accessory Pack",
     maker: "Dyzplastic",
     year: "2024",
@@ -66,10 +92,10 @@ const productData = {
       "media/products/dino/GGOEGFDA213699-3.webp"
     ]
   },
-  "dino-accessory-googler": {
+  "figure-accessory-googler": {
     name: "Chrome Dino Googler Accessory Pack",
     maker: "Dyzplastic",
-    year: "2024",
+    year: "2023",
     id: "GGOEGFDA213799",
     price: "$14.00",
     synopsis: "Chrome Dino is ready for work! Decked out with a lanyard, handy backpack and the world-famous Noogler cap, this little dino is ready to tackle anything.",
@@ -79,10 +105,259 @@ const productData = {
       "media/products/dino/GGOEGFDA213799-3.webp"
     ]
   },
-  "dino-patch": {
+  "glow-stickers": {
+    name: "Chrome Dino Glow-in-the-Dark Sticker",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGOKA212399",
+    price: "$2.00",
+    synopsis: "Turn off the lights! This Chrome Dino sticker really has it glowin' on, day or night!",
+    images: [
+      "media/products/dino/GGOEGOKA212399-1.webp",
+      "media/products/dino/GGOEGOKA212399-2.webp",
+      "media/products/dino/GGOEGOKA212399-3.webp",
+      "media/products/dino/GGOEGOKA212399-4.webp",
+      "media/products/dino/GGOEGOKA212399-5.webp",
+      "media/products/dino/GGOEGOKA212399-6.webp"
+    ]
+  },
+  "wood-magnet": {
+    name: "Chrome Dino Eco Wood Magnet",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGOBB216099",
+    price: "$7.00",
+    synopsis: "Take your fridge from blah to rahhhrr with a fun Chrome Dino magnet, made out of recycled wood fiber.",
+    images: [
+      "media/products/dino/GGOEGOBB216099-1.webp",
+      "media/products/dino/GGOEGOBB216099-2.webp",
+      "media/products/dino/GGOEGOBB216099-3.webp"
+    ]
+  },
+  "game-over-mug": {
+    name: "Chrome Dino Rope Lanyard",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGDWB211499",
+    price: "$14.00",
+    synopsis: "Start your morning off with a jolt of energy with this playful Chrome Dino mug caffeine not needed!",
+    images: [
+      "media/products/dino/GGOEGDWB211499-1.webp",
+      "media/products/dino/GGOEGDWB211499-2.webp",
+      "media/products/dino/GGOEGDWB211499-3.webp"
+    ]
+  },
+  "rope-lanyard": {
+    name: "Chrome Dino Rope Lanyard",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGOBB211299",
+    price: "$7.00",
+    synopsis: "Always on the run? So is the Chrome Dino! Make sure you grab this handy lanyard to keep your keys, badge and more easily accessible.",
+    images: [
+      "media/products/dino/GGOEGOBB211299-1.webp",
+      "media/products/dino/GGOEGOBB211299-2.webp",
+      "media/products/dino/GGOEGOBB211299-3.webp"
+    ]
+  },
+  "unisex-shirt": {
+    name: "Chrome Dino Glow-in-the-Dark Unisex Tee",
+    maker: "Robertson Marketing",
+    year: "2023",
+    id: "GGOEGXXX2075",
+    price: "$30.00",
+    synopsis: "We heard ya... and here is it... a Chrome Dino glow-in-the-dark tee just for adults! It's time to get glowin'!",
+    images: [
+      "media/products/dino/GGOEGXXX2075-1.webp",
+      "media/products/dino/GGOEGXXX2075-2.webp",
+      "media/products/dino/GGOEGXXX2075-3.webp"
+    ]
+  },
+  "party-card": {
+    name: "Chrome Dino Party Time Greeting Card",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGCOGCBA196099",
+    price: "$6.00",
+    synopsis: "This Chrome Dino is ready to party! You’ll be sure to bring a smile to someone’s face with this joyful card.",
+    images: [
+      "media/products/dino/GGCOGCBA196099-1.webp",
+      "media/products/dino/GGCOGCBA196099-2.webp",
+      "media/products/dino/GGCOGCBA196099-3.webp"
+    ]
+  },
+  "camp-shirt": {
+    name: "Chrome Dino Camp Shirt",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGXXX2003",
+    price: "$86.00",
+    synopsis: "Holy Moly! We've outdone ourselves and designed the coolest Chrome Dino camp shirt around. Prepare for oohs and aahs whenever you walk in the room.",
+    images: [
+      "media/products/dino/GGOEGXXX2003-1.webp",
+      "media/products/dino/GGOEGXXX2003-2.webp",
+      "media/products/dino/GGOEGXXX2003-3.webp"
+    ]
+  },
+  "enamel-pin": {
+    name: "Chrome Dino Enamel Pin",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGCBB203299",
+    price: "$7.00",
+    synopsis: "Jump for joy! This playful enamel pin of the Chrome Dino will perk up any lapel.",
+    images: [
+      "media/products/dino/GGOEGCBB203299-1.webp",
+      "media/products/dino/GGOEGCBB203299-2.webp",
+      "media/products/dino/GGOEGCBB203299-3.webp"
+    ]
+  },
+  "holiday-sweater": {
+    name: "Chrome Dino Holiday Sweater Ornament",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGFBA202499",
+    price: "$12.00",
+    synopsis: "Riddle: What sweater suits everyone and looks good in all types of weather? Answer: Our Chrome Dino sweater ornament! Scoop this limited-edition cutie while supplies last.",
+    images: [
+      "media/products/dino/GGOEGFBA202499-1.webp",
+      "media/products/dino/GGOEGFBA202499-2.webp",
+      "media/products/dino/GGOEGFBA202499-3.webp"
+    ]
+  },
+  "holiday-scarf": {
+    name: "Chrome Dino Holiday Scarf",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGAEA200799",
+    price: "$25.00",
+    synopsis: "Wrap yourself in a hug with this joyful Chrome Dino holiday scarf. Whenever the temperature starts to dip, you’ll be well prepared with this scarf nearby.",
+    images: [
+      "media/products/dino/GGOEGAEA200799-1.webp",
+      "media/products/dino/GGOEGAEA200799-2.webp",
+      "media/products/dino/GGOEGAEA200799-3.webp"
+    ]
+  },
+  "holiday-socks": {
+    name: "Chrome Dino Holiday Sock",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGAEA199699",
+    price: "$18.00",
+    synopsis: "You won’t stop smiling while wearing these festive Google socks. Perfect for any time of the year or whenever you want to spread a little joy!",
+    images: [
+      "media/products/dino/GGOEGAEA199699-1.webp",
+      "media/products/dino/GGOEGAEA199699-2.webp",
+      "media/products/dino/GGOEGAEA199699-3.webp"
+    ]
+  },
+  "recycled-cap": {
+    name: "Chrome Dino Recycled Cap",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGHPB175810",
+    price: "$21.00",
+    synopsis: "We’re loving this fun cap featuring one of our favorite Dinos (don’t worry Stan, we love you too)!  Plus, this hat is made from certified recycled material. How cool is that?",
+    images: [
+      "media/products/dino/GGOEGHPB175810-1.webp",
+      "media/products/dino/GGOEGHPB175810-2.webp",
+      "media/products/dino/GGOEGHPB175810-3.webp"
+    ]
+  },
+  "ultralight-sweater": {
+    name: "Chrome Dino Ultralight Sweatshirt",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGXXX1952",
+    price: "$58.00",
+    synopsis: "Make your day a bit cheerier with our Chrome Dino embroidered sweatshirt. The ultralight fabric makes it easy to sport our favorite pterodactyl-dodger any day of the year!",
+    images: [
+      "media/products/dino/GGOEGXXX1952-1.webp",
+      "media/products/dino/GGOEGXXX1952-2.webp",
+      "media/products/dino/GGOEGXXX1952-3.webp"
+    ]
+  },
+  "toddler-tee": {
+    name: "Chrome Dino Glow-in-the-Dark Toddler Tee",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGXXX1959",
+    price: "$25.00",
+    synopsis: "Kids will quite literally light up the room in this amazingly cool glow-in-the-dark Chrome Dino tee. Ready, set, glow!",
+    images: [
+      "media/products/dino/GGOEGXXX1959-1.webp",
+      "media/products/dino/GGOEGXXX1959-2.webp",
+      "media/products/dino/GGOEGXXX1959-3.webp"
+    ]
+  },
+  "keyboard-sticker": {
+    name: "Chrome Dino Keychain",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGFKA194899",
+    price: "$2.00",
+    synopsis: "Add some character to your keyboard! This small Chrome Dino sticker will be sure to bring some joy next time you’re at your computer.",
+    images: [
+      "media/products/dino/GGOEGFKA194899-1.webp",
+      "media/products/dino/GGOEGFKA194899-2.webp",
+      "media/products/dino/GGOEGFKA194899-3.webp"
+    ]
+  },
+  "keychain": {
+    name: "Chrome Dino Keychain",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGCBA195199",
+    price: "$8.00",
+    synopsis: "Take the Chrome Dino out for a new adventure on your backpack or car keys with this cute keychain. We're sure Dino needs a break from jumping over cacti and would like to see some new places!",
+    images: [
+      "media/products/dino/GGOEGCBA195199-1.webp",
+      "media/products/dino/GGOEGCBA195199-2.webp",
+      "media/products/dino/GGOEGCBA195199-3.webp"
+    ]
+  },
+  "zipper-pull": {
+    name: "Chrome Dino Zipper Pull",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGCBA180699",
+    price: "$5.00",
+    synopsis: "Upgrade your gear with this Chrome Dino zipper pull.  This tab is the perfect way to show brand love on your bag, backpack or jacket!",
+    images: [
+      "media/products/dino/GGOEGCBA180699-1.webp",
+      "media/products/dino/GGOEGCBA180699-2.webp",
+      "media/products/dino/GGOEGCBA180699-3.webp"
+    ]
+  },
+  "youth-tee": {
+    name: "Chrome Dino Glow-in-the-Dark Youth Tee",
+    maker: "Robertson Marketing",
+    year: "2022",
+    id: "GGOEGXXX1730",
+    price: "$23.00",
+    synopsis: "Kids will quite literally light up the room in this amazingly cool glow-in-the-dark Chrome Dino tee. Ready, set, glow!",
+    images: [
+      "media/products/dino/GGOEGXXX1730-1.webp",
+      "media/products/dino/GGOEGXXX1730-2.webp",
+      "media/products/dino/GGOEGXXX1730-3.webp"
+    ]
+  },
+  "sticker": {
+    name: "Chrome Dino Sticker Sheet",
+    maker: "Robertson Marketing",
+    year: "2021",
+    id: "GGOEGCKA176099",
+    price: "$3.00",
+    synopsis: "Chrome Dinos, cacti, pterodactyls, we’ve got you covered in the Chrome Dino department! Bonus Trivia: What is the significance of the high score?",
+    images: [ // "Trivia answer: 2014 was the year Chrome Dino was created!" ...the sticker sheet says "2021" 
+      "media/products/dino/GGOEGCKA176099-1.webp",
+      "media/products/dino/GGOEGCKA176099-2.webp"
+    ]
+  },
+  "patch": {
     name: "Chrome Dino Patch",
     maker: "Robertson Marketing",
-    year: "2019",
+    year: "2021",
     id: "GGOEGCBA173999",
     price: "$5.50",
     synopsis: "Add this embroidered Chrome Dino patch into your mix! With an easy to apply iron-on backing, it makes customizing anything from denim, hats, sweatshirts and more super simple!",
@@ -92,7 +367,59 @@ const productData = {
       "media/products/dino/GGOEGCBA173999-3.webp"
     ]
   },
-  "dino-set-dark": {
+  "socks": {
+    name: "Chrome Dino Socks",
+    maker: "Robertson Marketing",
+    year: "2021",
+    id: "GGOEGAKB165910",
+    price: "$18.00",
+    synopsis: "Once you slip on these Chrome Dino socks, you may feel the urge to run in the desert and jump over a cactus or two! Just watch out for any pterodactyls or GAME OVER!",
+    images: [
+      "media/products/dino/GGOEGAKB165910-1.webp",
+      "media/products/dino/GGOEGAKB165910-2.webp",
+      "media/products/dino/GGOEGAKB165910-3.webp"
+    ]
+  },
+  "food-container": {
+    name: "Chrome Dino Food Container",
+    maker: "Robertson Marketing",
+    year: "2021",
+    id: "GGOEGCBB165899",
+    price: "$38.00",
+    synopsis: "Long gone are the days of holding a cold pint of ice cream in your hands while binge-watching your favorite show! Keep cold or hot items their proper temperature for hours in this delightful Chrome Dino insulated container.",
+    images: [
+      "media/products/dino/GGOEGCBB165899-1.webp",
+      "media/products/dino/GGOEGCBB165899-2.webp",
+      "media/products/dino/GGOEGCBB165899-3.webp"
+     ]
+  },
+  "light-up-bottle": {
+    name: "Chrome Dino Light-Up Water Bottle",
+    maker: "Robertson Marketing",
+    year: "2021",
+    id: "GGOEGDHB163199",
+    price: "$24.00",
+    synopsis: "Kids and kids-at-heart will love this new water bottle! The laser engraved Chrome Dino is illuminated at the touch of a button to brighten your day. Plus, it features a convenient wrist strap so you can take it out and show it off!",
+    images: [
+      "media/products/dino/GGOEGDHB163199-1.webp",
+      "media/products/dino/GGOEGDHB163199-2.webp",
+      "media/products/dino/GGOEGDHB163199-3.webp"
+    ]
+  },
+  "marine-tee": {
+    name: "Chrome Dino Marine Layer Tee",
+    maker: "Robertson Marketing",
+    year: "2020",
+    id: "GGOEGXXX1194",
+    price: "$48.00",
+    synopsis: "This eco friendly and ultra soft shirt will have you ready to help your T-Rex buddy jump over cacti!",
+    images: [
+      "media/products/dino/GGOEGXXX1194-1.webp",
+      "media/products/dino/GGOEGXXX1194-2.webp",
+      "media/products/dino/GGOEGXXX1194-3.webp"
+    ]
+  },
+  "figure-set-dark": {
     name: "Chrome Dino Dark Mode Set",
     maker: "Dyzplastic",
     year: "2019",
@@ -109,46 +436,7 @@ const productData = {
       "media/products/dino/GGOEGABQ170299-7.webp"
     ]
   },
-  "light-up-bottle": {
-    name: "Chrome Dino Light-Up Water Bottle",
-    maker: "Robertson Marketing",
-    year: "2019",
-    id: "GGOEGDHB163199",
-    price: "$24.00",
-    synopsis: "Kids and kids-at-heart will love this new water bottle! The laser engraved Chrome Dino is illuminated at the touch of a button to brighten your day. Plus, it features a convenient wrist strap so you can take it out and show it off!",
-    images: [
-      "media/products/dino/GGOEGDHB163199-1.webp",
-      "media/products/dino/GGOEGDHB163199-2.webp",
-      "media/products/dino/GGOEGDHB163199-3.webp"
-    ]
-  },
-  "dino-socks": {
-    name: "Chrome Dino Socks",
-    maker: "Robertson Marketing",
-    year: "2019",
-    id: "GGOEGAKB165910",
-    price: "$18.00",
-    synopsis: "Once you slip on these Chrome Dino socks, you may feel the urge to run in the desert and jump over a cactus or two! Just watch out for any pterodactyls or GAME OVER!",
-    images: [
-      "media/products/dino/GGOEGAKB165910-1.webp",
-      "media/products/dino/GGOEGAKB165910-2.webp",
-      "media/products/dino/GGOEGAKB165910-3.webp"
-    ]
-  },
-  "marine-tee": {
-    name: "Chrome Dino Marine Layer Tee",
-    maker: "Robertson Marketing",
-    year: "2019",
-    id: "GGOEGXXX1194",
-    price: "$48.00",
-    synopsis: "This eco friendly and ultra soft shirt will have you ready to help your T-Rex buddy jump over cacti!",
-    images: [
-      "media/products/dino/GGOEGXXX1194-1.webp",
-      "media/products/dino/GGOEGXXX1194-2.webp",
-      "media/products/dino/GGOEGXXX1194-3.webp"
-    ]
-  },
-  "dino-set": {
+  "figure-set": {
     name: "Chrome Dino Set",
     maker: "Dyzplastic",
     year: "2019",
