1d0c6422df23558624dff6ebb7005c204b16f01a
Gemdation <gem@techie.com>
2025-07-20 23:24:12 -0500

    More Dino products

--- meta/site-database-dino.js
+++ meta/site-database-dino.js
@@ -79,6 +79,19 @@ const productData = {
       "media/products/dino/GGOEGFDA213799-3.webp"
     ]
   },
+  "dino-patch": {
+    name: "Chrome Dino Patch",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEGCBA173999",
+    price: "$5.50",
+    synopsis: "Add this embroidered Chrome Dino patch into your mix! With an easy to apply iron-on backing, it makes customizing anything from denim, hats, sweatshirts and more super simple!",
+    images: [
+      "media/products/dino/GGOEGCBA173999-1.webp",
+      "media/products/dino/GGOEGCBA173999-2.webp",
+      "media/products/dino/GGOEGCBA173999-3.webp"
+    ]
+  },
   "dino-set-dark": {
     name: "Chrome Dino Dark Mode Set",
     maker: "Dyzplastic",
@@ -94,6 +107,45 @@ const productData = {
       "media/products/dino/GGOEGABQ170299-5.webp",
       "media/products/dino/GGOEGABQ170299-6.webp",
       "media/products/dino/GGOEGABQ170299-7.webp"
+    ]
+  },
+  "light-up-bottle": {
+    name: "Chrome Dino Light-Up Water Bottle",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEGDHB163199",
+    price: "$24.00",
+    synopsis: "Kids and kids-at-heart will love this new water bottle! The laser engraved Chrome Dino is illuminated at the touch of a button to brighten your day. Plus, it features a convenient wrist strap so you can take it out and show it off!",
+    images: [
+      "media/products/dino/GGOEGDHB163199-1.webp",
+      "media/products/dino/GGOEGDHB163199-2.webp",
+      "media/products/dino/GGOEGDHB163199-3.webp"
+    ]
+  },
+  "dino-socks": {
+    name: "Chrome Dino Socks",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEGAKB165910",
+    price: "$18.00",
+    synopsis: "Once you slip on these Chrome Dino socks, you may feel the urge to run in the desert and jump over a cactus or two! Just watch out for any pterodactyls or GAME OVER!",
+    images: [
+      "media/products/dino/GGOEGAKB165910-1.webp",
+      "media/products/dino/GGOEGAKB165910-2.webp",
+      "media/products/dino/GGOEGAKB165910-3.webp"
+    ]
+  },
+  "marine-tee": {
+    name: "Chrome Dino Marine Layer Tee",
+    maker: "Robertson Marketing",
+    year: "2019",
+    id: "GGOEGXXX1194",
+    price: "$48.00",
+    synopsis: "This eco friendly and ultra soft shirt will have you ready to help your T-Rex buddy jump over cacti!",
+    images: [
+      "media/products/dino/GGOEGXXX1194-1.webp",
+      "media/products/dino/GGOEGXXX1194-2.webp",
+      "media/products/dino/GGOEGXXX1194-3.webp"
     ]
   },
   "dino-set": {
